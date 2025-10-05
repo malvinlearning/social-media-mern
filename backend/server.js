763d8c721546c3d52999fbe3143af0c3d89b4e07
hoxha-malvin <hoxhamalvin25@gmail.com>
2025-10-05 21:15:48 +0300

    Adding user logic

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,31 @@
 import express from "express";
-import authRoutes from "./routes/authRoutes.js";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import path from "path";
+
+import authRoutes from "./routes/authRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
+
 import connectMongoDB from "./db/connectMongoDB.js";
+
+import { v2 as cloudinary } from "cloudinary";
+
 dotenv.config();
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
+});
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true })); // to parse form data(urlencoded)
+app.use(cookieParser());
+
 app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
 
 app.listen(PORT, () => {
     console.log("Server running on port PORT");
